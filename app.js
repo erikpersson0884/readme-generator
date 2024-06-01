b2d8ce3e5fd99652010cd230c16dfe3f4fee02a5
Erik <erikpersson0884@gmail.com>
2024-06-01 19:07:04 +0200

    fixed repo name extraction bug and added footer

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 document.getElementById("githubLink").addEventListener("input", generateReadme);
 
+function extractRepoInfo(githubLink) {
+    const githubRegex = /(?:https?:\/\/)?(?:www\.)?github\.com\/([^\/\s]+)\/([^\/\s]+)/;
+    const githubMatch = githubLink.match(githubRegex);
+
+    // If it's a valid GitHub link, extract the username and repository name
+    if (githubMatch) {
+        return {
+            username: githubMatch[1],
+            repoName: githubMatch[2]
+        };
+    } else {
+        // If it's not a valid GitHub link, return the entire input
+        return null;
+    }
+}
+
 function generateReadme() {
     const githubLink = document.getElementById('githubLink').value.trim();
-    const repoName = extractRepoName(githubLink);
+    const repoInfo = extractRepoInfo(githubLink);
+
+    if (!repoInfo) {
+        document.getElementById('readmeResult').value = "Invalid GitHub link";
+        return;
+    }
+
+    const { username, repoName } = repoInfo;
 
     const readmeTemplate = `
 # ${repoName}
 ## ${repoName} is a repository for ...
 
 ![Github version](https://img.shields.io/badge/version-0.0.0-darkblue?style=flat-square)
-![GitHub repo size](https://img.shields.io/github/repo-size/${repoName}?color=blue&style=flat-square)
-![GitHub last commit](https://img.shields.io/github/last-commit/${repoName}?color=darkgreen&style=flat-square)
+![GitHub repo size](https://img.shields.io/github/repo-size/${username}/${repoName}?color=blue&style=flat-square)
+![GitHub last commit](https://img.shields.io/github/last-commit/${username}/${repoName}?color=darkgreen&style=flat-square)
 <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
 ![Github licence](https://img.shields.io/badge/licence-CC_BY_NC_SA_4.0-blueviolet?style=flat-square)
 </a>
@@ -19,19 +42,6 @@ function generateReadme() {
 **Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)**`;
 
     document.getElementById('readmeResult').value = readmeTemplate;
-}
-
-function extractRepoName(githubLink) {
-    const githubRegex = /(?:https?:\/\/)?(?:www\.)?github\.com\/([^\/\s]+)\/([^\/\s]+)/;
-    const githubMatch = githubLink.match(githubRegex);
-
-    // If it's a valid GitHub link, use the extracted repository name
-    if (githubMatch) {
-        return `${githubMatch[1]}/${githubMatch[2]}`;
-    } else {
-        // If it's not a valid GitHub link, use the entire input as the repository name
-        return githubLink;
-    }
 }
 
 function copyToClipboard() {
