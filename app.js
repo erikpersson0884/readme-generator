db86eab054b3efbdf0b04036bf90257a9d886ece
Erik <erikpersson0884@gmail.com>
2024-06-23 03:56:29 +0200

    added functionality to chose which part to include/exclude

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
 document.getElementById("githubLink").addEventListener("input", generateReadme);
+const readmeResult = document.getElementById('readmeResult');
+
+const inputButtonTitle = document.getElementById('includeTitle');
+const inputButtonDescription = document.getElementById('includeDescription');
+const inputButtonVersion = document.getElementById('includeVersion');
+const inputButtonRepoSize = document.getElementById('includeRepoSize');
+const inputButtonLastCommit = document.getElementById('includeLastCommit');
+const inputButtonLicense = document.getElementById('includeLicense');
+
+const inputOptions = [inputButtonTitle, inputButtonDescription, inputButtonVersion, inputButtonRepoSize, inputButtonLastCommit, inputButtonLicense];
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    inputOptions.forEach((inputOption) => {
+        inputOption.addEventListener('click', () => {
+            console.log(inputOption.checked);
+
+            generateReadme();
+            localStorage.setItem(inputOption.id, inputOption.checked);
+        });
+        const cstatus = localStorage.getItem(inputOption.id)
+        console.log(cstatus)
+
+        inputOption.checked = cstatus === 'true' ? true : false;
+    });
+});
+
+
 
 function extractRepoInfo(githubLink) {
     const githubRegex = /(?:https?:\/\/)?(?:www\.)?github\.com\/([^\/\s]+)\/([^\/\s]+)/;
@@ -17,6 +46,9 @@ function extractRepoInfo(githubLink) {
 }
 
 function generateReadme() {
+
+
+
     const githubLink = document.getElementById('githubLink').value.trim();
     const repoInfo = extractRepoInfo(githubLink);
 
@@ -27,25 +59,22 @@ function generateReadme() {
 
     const { username, repoName } = repoInfo;
 
-    const readmeTemplate = `
-# ${repoName}
-## ${repoName} is a repository for ...
+    readmeResult.value = 
+`${inputButtonTitle.checked ? `# ${repoName}` : ''}
+${inputButtonDescription.checked ? `## ${repoName} is...` : ''}
 
-![Github version](https://img.shields.io/badge/version-0.0.0-darkblue?style=flat-square)
-![GitHub repo size](https://img.shields.io/github/repo-size/${username}/${repoName}?color=blue&style=flat-square)
-![GitHub last commit](https://img.shields.io/github/last-commit/${username}/${repoName}?color=darkgreen&style=flat-square)
-<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
-![Github licence](https://img.shields.io/badge/licence-CC_BY_NC_SA_4.0-blueviolet?style=flat-square)
-</a>
+${inputButtonVersion.checked ? `![Github version](https://img.shields.io/badge/version-0.0.0-darkblue?style=flat-square)` : ''}
+${inputButtonRepoSize.checked ? `![GitHub repo size](https://img.shields.io/github/repo-size/${username}/${repoName}?color=blue&style=flat-square)` : ''}
+${inputButtonLastCommit.checked ? `![GitHub last commit](https://img.shields.io/github/last-commit/${username}/${repoName}?color=darkgreen&style=flat-square)` : ''}
+${inputButtonLicense.checked ? `<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/"> ![License](https://img.shields.io/badge/license-CC%20BY--NC--SA%204.0-lightgrey?style=flat-square)</a>
 
 ### License:
-**Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)**`;
+**Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)**` : ''}
 
-    document.getElementById('readmeResult').value = readmeTemplate;
+`;
 }
 
 function copyToClipboard() {
-    const readmeResult = document.getElementById('readmeResult');
     readmeResult.select();
     document.execCommand('copy');
 }
